e7bd4442c51197cf3fbefe23a09750ce53987245
*GeorgeCandal* <George3567@gmail.com>
2025-03-16 19:41:35 -0600

    Finished BusStop class

--- Stage 3/src/BusStop.java
+++ Stage 3/src/BusStop.java
@@ -1,12 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
- *
- * @author georg
+ * This class represents a bus stop. It contains one variable for the name.
+ * @author George Candal
  */
 public class BusStop {
+    
+    private String name;
+
+    /**
+     * Constructor for the class. Initializes the name.
+     * @param name The name of the bus stop
+     */
+    public BusStop(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the name of the bus stop.
+     * @return name The name of the bus stop
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the bus stop.
+     * @param name The name for the bus stop
+     */
+    public void setName(String name) {
+        this.name = name;
+    }   
     
 }
