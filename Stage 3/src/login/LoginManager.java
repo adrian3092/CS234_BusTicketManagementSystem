9e2d64d65bdf81d7997c2948ea3a0f17ac6dc091
*GeorgeCandal* <George3567@gmail.com>
2025-03-30 21:20:08 -0600

    Combined LoginMenu and LoginManager classes

--- Stage 3/src/login/LoginManager.java
+++ Stage 3/src/login/LoginManager.java
@@ -1,38 +1,38 @@
 package login;
 
 import login.Login;
-import login.LoginMenu;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
-
-
 /**
- *
+ * This class manages the login system. 
  * @author George Candal
  */
 public class LoginManager {
     
     private Scanner in;
     private ArrayList<Login> logins;
-    private LoginMenu loginMenu;
-    private String userName;
-    private String password;
+    private String enteredUsername;
+    private String enteredPassword;
 
+    /**
+     * Constructor for the class. Initializes the database of logins.
+     * @param in 
+     */
     public LoginManager(Scanner in) {
         this.in = in;
         logins = new ArrayList<>();
-        this.loginMenu = new LoginMenu(in);
-        userName = loginMenu.getUsername();
-        password = loginMenu.getPassword();
-        
     }
     
+    /**
+     * This checks the credentials by searching the login database for a
+     * matching username and password.
+     * @return True if username and password match
+     */
     public boolean checkCredentials() {
         for (Login login : logins) {
-            if (login.getUsername().equals(userName)) {
-                if (login.getPassword().equals(password)) {
+            if (login.getUsername().equals(enteredUsername)) {
+                if (login.getPassword().equals(enteredPassword)) {
                     return true;
                 } else {
                     System.out.println("Invalid password");
@@ -44,5 +44,14 @@ public class LoginManager {
     return false;
     }
     
-    
+    /**
+     * This displays the login menu and saves the username and password entered
+     * by the user.
+     */
+    public void loginMenu() {
+        System.out.println("Username: ");
+        enteredUsername = in.next();
+        System.out.println("Password: ");
+        enteredPassword = in.next();
+    }
 }
