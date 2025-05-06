7d68197fa14cbebdb94d0b9f3ce9221ccdbb3ecc
Precious Onojerame <preciousonoj@gmail.com>
2025-05-06 00:35:37 -0600

    design adjustments

--- Stage 4/src/main/ViewScheduleGUI.java
+++ Stage 4/src/main/ViewScheduleGUI.java
@@ -78,7 +78,7 @@ public class ViewScheduleGUI extends javax.swing.JFrame {
                                                        boolean hasFocus, int row, int column) {
             Component c = super.getTableCellRendererComponent(tbl, value, isSelected, hasFocus, row, column);
             if (isSelected) {
-                c.setBackground(new Color(102, 178, 255)); // Light blue
+                c.setBackground(Color.green); // Light blue
                 c.setForeground(Color.BLACK);
             } else {
                 c.setBackground(row % 2 == 0 ? Color.WHITE : new Color(245, 245, 245)); // Alternating colors
